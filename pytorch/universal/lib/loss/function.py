d4c6700fd927a42963dd8705c7877117ed24d7fe
rong <37278270+chaotiaor@users.noreply.github.com>
2020-10-28 14:30:16 +0800

    Add files via upload

---
+++ pytorch/universal/lib/loss/function.py
@@ -0,0 +1,193 @@
+"""
+定义几个常用的loss函数
+"""
+from torch.nn import functional as F
+from torch.optim import lr_scheduler
+import torch.nn as nn
+import torch
+
+
+def L1(p, label):
+    return F.l1_loss(p, label)
+
+
+def entropy(p, label):
+    return F.cross_entropy(p, label)
+
+
+def loss_kl(student_outputs, teacher_outputs, T):
+    """
+    """
+    kd_loss = nn.KLDivLoss()(F.log_softmax(student_outputs / T, dim=1),
+                             F.softmax(teacher_outputs / T, dim=1)) * T * T
+    return kd_loss
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        """
+        :param gamma:
+        :param alpha: 各类别权重系数，必须是一行n列的数组或者列表
+        :param size_average:
+        """
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, inputs, target):
+        if inputs.dim() > 2:
+            inputs = inputs.view(inputs.size(0), inputs.size(1), -1)  # N,C,H,W => N,C,H*W
+            inputs = inputs.transpose(1, 2)  # N,C,H*W => N,H*W,C
+            inputs = inputs.contiguous().view(-1, inputs.size(2))  # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(inputs, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = logpt.data.exp()
+
+        if self.alpha is not None:
+            if self.alpha.type() != inputs.data.type():
+                self.alpha = self.alpha.type_as(inputs.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * at
+
+        loss = -1 * (1 - pt) ** self.gamma * logpt
+
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+
+class SigmoidLoss(nn.Module):
+    def __init__(self):
+        super(SigmoidLoss, self).__init__()
+        self.loss = nn.BCEWithLogitsLoss()
+
+    def forward(self, inputs, target):
+        loss = self.loss(inputs, target)
+        return loss
+
+
+def flatten(tensor):
+    """Flattens a given tensor such that the channel axis is first.
+    The shapes are transformed as follows:
+       (N, C, D, H, W) -> (C, N * D * H * W)
+    """
+    c = tensor.size(1)
+    # new axis order
+    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
+    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
+    transposed = tensor.permute(axis_order)
+    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
+    return transposed.contiguous().view(c, -1)
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.epsilon = 1e-5
+
+    def forward(self, output, target):
+        assert output.size() == target.size(), "'input' and 'target' must have the same shape"
+        # output = F.softmax(output, dim=1)
+        output = F.sigmoid(output)
+        output = flatten(output)
+        target = flatten(target)
+        # intersect = (output * target).sum(-1).sum() + self.epsilon
+        # denominator = ((output + target).sum(-1)).sum() + self.epsilon
+
+        intersect = (output * target).sum(-1)
+        denominator = (output + target).sum(-1)
+        dice = intersect / denominator
+        dice = torch.mean(dice)
+        return 1 - dice
+
+
+def get_scheduler(optimizer, opt):
+    """Return a learning rate scheduler
+    Parameters:
+        optimizer          -- the optimizer of the network
+        opt (option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions．　
+                              opt.lr_policy is the name of learning rate policy: linear | step | plateau | cosine
+    For 'linear', we keep the same learning rate for the first <opt.n_epochs> epochs
+    and linearly decay the rate to zero over the next <opt.n_epochs_decay> epochs.
+    For other schedulers (step, plateau, and cosine), we use the default PyTorch schedulers.
+    See https://pytorch.org/docs/stable/optim.html for more details.
+    """
+    if opt.lr_policy == 'linear':
+        def lambda_rule(epoch):
+            lr_l = 0.95 ** (epoch / 20)
+            return lr_l
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+    elif opt.lr_policy == 'step':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.5)
+    elif opt.lr_policy == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+    elif opt.lr_policy == 'cosine':
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.n_epochs, eta_min=0)
+    else:
+        return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
+    return scheduler
+
+
+if __name__ == '__main__':
+    """
+    
+    """
+    import torch
+    import cv2
+    import numpy as np
+    from lib.core.portraitnet.api import MobileNetV2
+    # for i in range(299, 400):
+    #     if i % 32 == 0:
+    #         print(i)
+    # print()
+    # for i in range(199, 300):
+    #     if i % 32 == 0:
+    #         print(i)
+    # w = 352
+    # h = 256
+    # classes = 3
+    #
+    # model = MobileNetV2(classes)
+    # img = torch.rand((1, 3, h, w))
+    # mask_copy = np.zeros((1, classes, h, w))
+    #
+    # mask = cv2.imread('C:\\Users\\mwrd\\Desktop\\old\\eyelid_gland_data\\9-28_masks\\00000000.png', 0)
+    # mask = cv2.resize(mask, (w, h), interpolation=cv2.INTER_NEAREST)
+    # mask = np.array(mask)
+    # mask = np.expand_dims(mask, 0)
+    # mask = torch.from_numpy(mask).long()
+    # focal = FocalLoss(1, [0.2, 0.5, 0.3])
+    # out = model(img)
+    # print(out.shape)
+    # # loss_ = nn.CrossEntropyLoss()
+    # # a = loss_(out, mask)
+    # # print(a)
+    #
+    # # loss_ = nn.BCEWithLogitsLoss()
+    # loss_ = DiceLoss()
+    # # mask_copy[:, 0, mask == 0] = 1
+    # # mask_copy[:, 1, mask == 1] = 1
+    # # mask_copy[:, 2, mask == 2] = 1
+    #
+    # label = nn.functional.one_hot(mask, 3)
+    # label = label.permute(2, 0, 1)
+    # label = label.unsqueeze(0).float()
+    #
+    # # mask_copy = torch.from_numpy(mask_copy).float()
+    # # print(torch.sum(mask_copy - label))
+    # a = loss_(out, label)
+    # print(a)
+
+
+
+
+
+
+
+
+
